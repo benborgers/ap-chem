8b5b7f66de416256e72a2bd7fa6e2149efb41f74
benborgers <borgersbenjamin@gmail.com>
2019-11-12 17:57:37 -0500

    Head: add open graph meta tags

--- src/components/Head.js
+++ src/components/Head.js
@@ -7,6 +7,13 @@ export default ({ title, description }) => {
       <title>{title}</title>
       <meta name="description" content={description} />
 
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content="https://images.unsplash.com/photo-1532094349884-543bc11b234d?fit=crop&w=1200&h=630&q=80&fm=png" />
+
+      <meta property="twitter:card" content="summary_large_image" />
+
       <link rel="icon" href="/icon.png" />
     </Helmet>
   )
